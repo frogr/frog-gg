e161dc6acb9b7086dc61952e0e43bf4dad99b5ae
Austin F <austindanielfrench@gmail.com>
2017-10-24 16:43:18 -0700

    wtf i actually managed to get exports and imports working I feel so powerful

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Splash from "./Splash.js";
 
 class App extends Component {
   render() {
-    return (
-      <div className="Splash">
-        <div className="Splash-title">
-          <h1>fr0g.gg</h1>
-          <div className="input" />
-          <form action="#" />
-          <input type="text" />
-          <input type="submit" id="submit" />
-        </div>
-      </div>
-    );
+    return;
   }
 }
 
---
+++ src/Splash.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Splash extends Component {
+  render() {
+    return (
+      <div className="Splash">
+        <div className="Splash-title">
+          <h1>fr0g.gg</h1>
+          <div className="input" />
+          <form action="#" />
+          <input type="text" />
+          <input type="submit" id="submit" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Splash;
