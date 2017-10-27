fd68a734e59be652dbc9fc1475589bc17cab936e
Austin F <austindanielfrench@gmail.com>
2017-10-26 21:14:58 -0700

    Questions input field added needs style!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Splash from "./Splash.js";
+import Questions from "./Questions.js";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Splash />
+        <Questions />
       </div>
     );
   }
---
+++ src/Questions.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Questions = () => {
+  return [
+    <div className="fr0g__questions">
+      <form action="#">
+        <input placeholder="Full name" /> <br />
+        <input placeholder="League of Legends IGN" /> <br />
+        <input placeholder="Age" /> <br />
+        <input placeholder="Rank" /> <br />
+        <input placeholder="Email" /> <br />
+      </form>
+    </div>
+  ];
+};
+
+export default Questions;
