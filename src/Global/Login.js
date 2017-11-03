3ee4e87b7575ebe11210814d91556f516a7a57c9
Austin F <austindanielfrench@gmail.com>
2017-11-03 14:14:51 -0700

    frontend login form created, not linked to the site. it is its own dumb component for now, will put it together later. mostly focused on getting everything fleshed out, then linking it up another time

--- src/Global/Login.js
+++ src/Global/Login.js
@@ -1,2 +1,43 @@
 import React from "react";
-import {} from "react-bootstrap";
+import {
+  Form,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  Button,
+  Col
+} from "react-bootstrap";
+
+export const Login = () => {
+  return (
+    <Form horizontal className="fr0g__form">
+      <FormGroup controlId="emailInput">
+        <Col componentClass={ControlLabel} sm={2}>
+          Email
+        </Col>
+        <Col sm={10}>
+          <FormControl type="email" placeholder="email" id="fr0g__form-input" />
+        </Col>
+      </FormGroup>
+
+      <FormGroup controlId="passwordInput">
+        <Col componentClass={ControlLabel} sm={2}>
+          Password
+        </Col>
+        <Col sm={10}>
+          <FormControl
+            type="password"
+            placeholder="password"
+            id="fr0g__form-input"
+          />
+        </Col>
+      </FormGroup>
+
+      <FormGroup>
+        <Col smOffset={2} sm={10}>
+          <Button type="submit">log in</Button>
+        </Col>
+      </FormGroup>
+    </Form>
+  );
+};
--- src/Splash/SplashApp.js
+++ src/Splash/SplashApp.js
@@ -3,16 +3,18 @@ import SplashBody from "./SplashBody.js";
 import Registration from "./Registration.js";
 import { Footer } from "../Global/Footer.js";
 import { NavBar } from "../Global/Navbar.js";
+import { Login } from "../Global/Login.js";
 import "./Splash.css";
 
 class SplashApp extends Component {
   render() {
     return (
       <div>
-        <NavBar />
+        {/* <NavBar />
         <SplashBody />
         <Registration />
-        <Footer />
+        <Footer /> */}
+        <Login />
       </div>
     );
   }
