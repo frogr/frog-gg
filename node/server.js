101049989b2c896d4e5b53790d2f2b0126a79526
Austin F <austindanielfrench@gmail.com>
2017-12-13 01:17:12 -0800

    I have to rewrite my entire API but at least I learned how to make a fetch call in javascript!

--- node/server.js
+++ node/server.js
@@ -1,7 +1,8 @@
 process.env.LEAGUE_API_PLATFORM_ID = 'na1';
 
 const LeagueJs = require('leaguejs');
-const api = new LeagueJs('RGAPI-ed1e7b48-1319-43c0-9b40-c7bb8743ff3c');
+const fetch = require('node-fetch');
+const api = new LeagueJs('RGAPI-cc5110d1-1aec-44e7-af28-6b03c6e0a994');
 
 const getAccountInfoBySummoner = summoner => {
   api.Summoner.gettingByName(summoner)
@@ -41,8 +42,13 @@ const getRecentMatchesBySummoner = summoner => {
     .then(account => {
       api.Match.gettingRecentListByAccount(account.accountId).then(match => {
         console.log('--RECENT GAMES--');
-
-        );
+        let championEndPointURL =
+          'https://na1.api.riotgames.com/lol/static-data/v3/champions/' +
+          match.matches[0].champion +
+          '?locale=en_US&api_key=RGAPI-7ccfe7bb-d774-4d4f-8537-6b8ed9d01a9a';
+        fetch(championEndPointURL).then(function(json) {
+          console.log(json);
+        });
       });
     })
     .catch(err => {
@@ -68,15 +74,29 @@ const getChampion = (i, id) => {
   });
 };
 
+const test = cid => {
+  const url =
+    'https://na1.api.riotgames.com/lol/static-data/v3/champions/67?locale=en_US&api_key=RGAPI-6ddc5f2e-65e8-41d7-876c-4eef09d94a9a';
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      return data;
+    })
+    .catch(e => {
+      console.log('!E', e);
+    });
+};
+test();
 // getAccountInfoBySummoner('im frog');
-getRecentMatchesBySummoner('im frog');
+// getRecentMatchesBySummoner('im frog');
 // getLiveGameBySummoner('ourania');
 // getChampion(67);
 
 //TODO need to figure out a way to assign champion IDs and summoner spell IDs and item IDs to their A) images and B) correct data
 // e.g. champ id 1 = "Annie" || item id x = "Dorans Blade" || item id x image = "doransblade.jpg" ||
 
-// big find! the ids are plaintext in the endpoint. something like JSON(summonerurl/:id/) may work! 
+// big find! the ids are plaintext in the endpoint. something like JSON(summonerurl/:id/) may work!
 
 // if this information could show up on the webpage, the project is 1/2 doransblade
 // after that, need to work on creating champion statistics (how the fk does opgg do that)
